968bbb10f0d799ddc7049bc6efe6d5ed713e602c
CHEBSB <chebsb229@gmail.com>
2019-09-27 09:58:54 +0800

    version 2: add some more steps that are alo basic

--- tetris.cpp
+++ tetris.cpp
@@ -3,26 +3,29 @@
 using namespace std;
 
 Typedef struct {	/* a 2-element struct to indicate
-					a certain position in a matrix */
+			a certain position in a matrix */
 	int row;
 	int col;
-} PosInMatrix;
+} MatrixPos;
 
 class tetris {
 public:
 	tetris(char *tetrisType, int initCol, int NumOfGameRow);
 private:
-	int cornerCol;				/* column (horizontal position)
-	of the lower left corner in the gameMatrix	*/
-	int cornerRow;				/* current row (vertical position) 
+	int cornerCol;		/* column (horizontal position)
+	of the lower left corner in the gameMatrix */
+	int cornerRow;		/* current row (vertical position) 
 	of the lower left corner in the gamaMatrix */
-	PosInMatrix geoshape[4];	/* indicate the 4 positions 
+	MatrixPos geoshape[4];	/* indicate the 4 positions 
 	that has a square cell */
 };
 
+void Fall(tetris& newCome);	/* whenever there is a new
+tetris coming from above, let it fall until hit something */
+
 int main()
 {
-	bool **gameMatrix;	
+	bool **gameMatrix;	// bool type: 1 for occupied space
 	int gameMatrixRow, gameMatrixCol;
 
 	// reading size from test case
@@ -30,7 +33,12 @@ int main()
 	// allocate memory for gameMatrix
 	gameMatrix = new *bool[gameMatrixRow];
 	for (int i = 0; i < gameMatrixRow)
-		gameMatrix = new bool[gameMatrixCol];
+		gameMatrix[i] = new bool[gameMatrixCol];
+	
 
+	// finally, delete memory
+	for (int i = 0; i < gameMatrixRow)
+		delete [] gameMatrix[i];
+	delete gameMatrix;
 	return void;
 }
