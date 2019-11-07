87fac2ec40019b1c8a6db1ff122aebb75b7a7dce
CHEBSB <chebsb229@gmail.com>
2019-11-07 09:33:15 +0800

    Proj 2 - 6: rewrite many parts, rebuilding PointWiseTraverse

--- CleanBot.cpp
+++ CleanBot.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class stack {
 public:
 	void push(const int);
-	void pop() { ~Arr[--size];  }
+	void pop() { ~Arr[--size]; }
 	int top() { return Arr[size - 1]; }
 	bool Empty() { return size ? 0 : 1; }
 	stack() : size(0), capa(10) { Arr = new int[10]; }
@@ -29,7 +29,6 @@ private:
 	bool* wayHome;	/* if Bot want to go back to R 
 	with shortest path, which direction may it take? 
 	0, 1, 2, 3 for up, down, left, right */
-	// to be Added...
 }; 
 
 class robot {
@@ -46,18 +45,19 @@ private:
 	int rowPos, colPos;	// current position of bot
 	int RowSize, ColSize;	// array size
 	int Rrow, Rcol;		// position of R
-	cell* Cur(int, int);
+	cell* Cur(const int, const int);
 	cell* Cur() { return Cur(rowPos, colPos); }
-	cell* Up(int, int);
+	cell* Up(const int, const int);
 	cell* Up() { return Up(rowPos, colPos); }
-	cell* Down(int, int);
+	cell* Down(const int, const int);
 	cell* Down() { return Down(rowPos, colPos); }
-	cell* Left(int, int);
+	cell* Left(const int, const int);
 	cell* Left() { return Left(rowPos, colPos); }
-	cell* Right(int, int);
+	cell* Right(const int, const int);
 	cell* Right() { return Right(rowPos, colPos); }
 	void afterMove();	// after robot move a step, call this function
-	void Traverse();		// first step of traversal
+	int FindWayBack(const int, const int);
+	void Traverse(const int);		// first step of traversal
 	void PointWiseTraverse(int, int);	/* find the
 	optimized way to clean a particular point */
 };
@@ -100,36 +100,42 @@ robot::robot() {
 	for (i = 0; i < RowSize; i++)	
 		for (j = 0; j < ColSize; j++) {
 			data >> c;
-			if (c == '0') {	// need to clean
+			switch (c) {
+			case '0': 	// need to clean
 				map[i][j] = cell(false, 1000000);
 				map[i][j].wayHome = new bool[4];
-			} else if (c == '1') {	// obstacle
+				break;
+			case '1': 	// obstacle
 				map[i][j] = cell(true, -1);
-			} else if (c == 'R') {
+				break;
+			case 'R': 
 				map[i][j] = cell(true, 0);
 				rowPos = Rrow = i;
 				colPos = Rcol = j;
-			}	else throw "Error";
+				break;
+			default:
+				throw "Error";
+			}
 		}
 	data.close();
 }
 
-cell* robot::Cur(int r, int c) {
+cell* robot::Cur(const int r, const int c) {
 	return &(map[r][c]);
 }
-cell* robot::Up(int r, int c) {
+cell* robot::Up(const int r, const int c) {
 	if (r - 1 >= 0) return &(map[r - 1][c]);
 	return NULL;
 }
-cell* robot::Down(int r, int c) {
+cell* robot::Down(const int r, const int c) {
 	if (r + 1 < RowSize) return &(map[r + 1][c]);
 	return NULL;
 }
-cell* robot::Left(int r, int c) {
+cell* robot::Left(const int r, const int c) {
 	if (c - 1 >= 0) return &(map[r][c - 1]);
 	return NULL;
 }
-cell* robot::Right(int r, int c) {
+cell* robot::Right(const int r, const int c) {
 	if (c + 1 < ColSize) return &(map[r][c + 1]);
 	return NULL;
 }
@@ -195,116 +201,91 @@ void robot::afterMove()
 
 void robot::Traversal()
 {
-	Traverse();
+	Traverse(-1);
 	for (int i = 0; i < RowSize; i++)
 		for (int j = 0; j < ColSize; j++)
 			if (!map[i][j].done)
 				PointWiseTraverse(i, j);
 }
 
-void robot::Traverse()
+int robot::FindWayBack(const int r, const int c)
 {
+	int bestOption = -1, secChoice = -1;
+
+	if (Cur(r, c)->wayHome[0]) {
+		if (!Up(r, c)->done) bestOption = 0;
+		else secChoice = 0;
+	} // up
+	if (Cur(r, c)->wayHome[1] && bestOption == -1) {
+		if (!Down(r, c)->done)	bestOption = 1;
+		else if (secChoice == -1)
+			secChoice = 1;
+	} // down
+	if (Cur(r, c)->wayHome[2] && bestOption == -1) {
+		if (!Left(r, c)->done)	bestOption = 2;
+		else if (secChoice == -1)
+			secChoice = 2;
+	} // left
+	if (Cur(r, c)->wayHome[3] && bestOption == -1) {
+		if (!Right(r, c)->done) bestOption = 3;
+		else if (secChoice == -1)
+			secChoice = 3;
+	} // right
+
+	if (bestOption != -1) return bestOption;
+	return secChoice;
+}
+
+void robot::Traverse(const int from)
+{
+	switch (from) {
+	case 0: rowPos--; 
+		break;
+	case 1: rowPos++; 
+		break;
+	case 2: colPos--; 
+		break;
+	case 3: colPos++; 
+	}
 	afterMove();
 	if (!WAYHOME) { // still going deeper
-		if (!Up()->done) {
-			rowPos--;	// go up
-			Traverse();
-		}
-		else if (!Down()->done) {
-			rowPos++;	// go down
-			Traverse();
-		}
-		else if (!Left()->done) {
-			colPos--;	// go left
-			Traverse();
-		}
-		else if (!Right()->done) {
-			colPos++;	// go right
-			Traverse();
-		}
+		if (!Up()->done) 
+			Traverse(0);	// go up	
+		else if (!Down()->done) 
+			Traverse(1);	// go down
+		else if (!Left()->done) 
+			Traverse(2);	// go left	
+		else if (!Right()->done) 
+			Traverse(3);	// go right
 		else {
 			if (rowPos == Rrow && colPos == Rcol) 
 				return;
 			else {
-				if (Cur()->wayHome[0]) {
-					rowPos--; // up
-					Traverse();
-				} else if (Cur()->wayHome[1]) {
-					rowPos++; // down
-					Traverse();
-				} else if (Cur()->wayHome[2]) {
-					colPos--; // left
-					Traverse();
-				} else if (Cur()->wayHome[3]) {
-					colPos++; // right
-					Traverse();
-				} else throw 404;
+				if (Cur()->wayHome[0]) 
+					Traverse(0);	// go up
+				else if (Cur()->wayHome[1]) 
+					Traverse(1);	// go down
+				else if (Cur()->wayHome[2]) 
+					Traverse(2);	// go left
+				else if (Cur()->wayHome[3]) 
+					Traverse(3);	// go right
+				else throw 404;
 			}
 		}
 	}
-	else {		// on its way home
-		int bestOption = -1, secChoice = -1;
-
-		// up
-		if (Cur()->wayHome[0]) {
-			if (!Up()->done) bestOption = 0;
-			else secChoice = 0;
-		}
-		// down
-		if (Cur()->wayHome[1] && bestOption == -1) {
-			if (!Down()->done)	bestOption = 1;
-			else if (secChoice == -1)
-				secChoice = 1;
-		} 
-		// left
-		if (Cur()->wayHome[2] && bestOption == -1) {
-			if (!Left()->done)	bestOption = 2;
-			else if (secChoice == -1)
-				secChoice = 2;
-		}
-		// right
-		if (Cur()->wayHome[3] && bestOption == -1) {
-			if (!Right()->done) bestOption = 3;
-			else if (secChoice == -1)
-				secChoice = 3;
-		}
-		
-		
-		switch (bestOption) {
+	else {		// on its way home		
+		switch (FindWayBack(rowPos, colPos)) {
 		case 0: // go up
-			rowPos--;
-			Traverse();
+			Traverse(0);
 			break;
 		case 1: // go down
-			rowPos++;
-			Traverse();
+			Traverse(1);
 			break;
 		case 2: // go left
-			colPos--;
-			Traverse();
+			Traverse(2);
 			break;
 		case 3: // go right
-			colPos++;
-			Traverse();
-			break;
-		default: 
-			switch (secChoice) {
-				case 0:	// go up
-					rowPos--;
-					Traverse();
-					break;
-				case 1:	// go down
-					rowPos++;
-					Traverse();
-					break;
-				case 2:	// go left
-					colPos--;
-					Traverse();
-					break;
-				case 3:	// go right
-					colPos++; 
-					Traverse();		
-			}
+			Traverse(3);
 		}
 	}
 }
@@ -313,5 +294,43 @@ void robot::PointWiseTraverse(int r, int c)
 {
 	stack S;
 
-	      		
+	// find the shortest path from R to this point
+	while (r != Rrow || c != Rcol) {
+		switch (FindWayBack(r, c)) {
+		case 0: // go up
+			r--;
+			S.push(1);	/* when Bot is heading from R
+			to this point, it has to go down */
+			break;
+		case 1: // go down
+			r++;
+			S.push(0);
+			break;
+		case 2: // go left
+			c--;
+			S.push(3);
+			break;
+		case 3: // go right
+			c++;
+			S.push(2);
+		}
+	}
+	// at this moment, Bot should be at R
+	for (; !S.Empty(); afterMove(), S.pop()) {
+		switch (S.top()) {
+		case 0:
+			rowPos--;
+			break;
+		case 1:
+			rowPos++;
+			break;
+		case 2:
+			colPos--;
+			break;
+		case 3:
+			colPos++;
+		}
+	}
+	// Now the bot should be at the initial given position 
+
 }
